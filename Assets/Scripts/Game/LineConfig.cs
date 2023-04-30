773ae2e405232d1f3468d80a809499e7aa59f98d
brockstar512 <60516979+brockstar512@users.noreply.github.com>
2023-04-29 21:28:01 -0500

    tfind the size and angle

--- Assets/Scripts/Game/LineConfig.cs
+++ Assets/Scripts/Game/LineConfig.cs
@@ -7,55 +7,20 @@ using static UnityEngine.GraphicsBuffer;
 
 public class LineConfig : MonoBehaviour
 {
-    [SerializeField] RectTransform rUpCorner;
-    [SerializeField] RectTransform lUpCorner;
-    [SerializeField] RectTransform rMiddleCorner;
-    [SerializeField] RectTransform lMiddleCorner;
-    [SerializeField] RectTransform rDownCorner;
-    [SerializeField] RectTransform lDownCorner;
-    [SerializeField] RectTransform midDownCorner;
-    [SerializeField] RectTransform midUpCorner;
-    Transform target;
-
-
-    public Quaternion GetRotation(Vector3 relativePos)
+    [SerializeField] RectTransform holder;
+    [SerializeField] RectTransform me;
+    private void Awake()
     {
-        Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
-
-        return rotation;
-
-    }
-
-    
-
-    public Vector2 GetLength(RectTransform start, RectTransform finish, RectTransform image)
-    {
-
-        //Vector3 relativePos = finish.position - start.position;
-        Vector2 relativePos = (finish.anchoredPosition - start.anchoredPosition).normalized; ;
-        //Vector2 dir = (dotPositionB - dotPositionA).normalized;
-        float distance = Vector2.Distance(start.anchoredPosition, finish.anchoredPosition);
-        image.anchorMin = new Vector2(0, 1);
-        image.anchorMax = new Vector2(0, 1);
-        image.sizeDelta = new Vector2(distance, 10f);
-        image.anchoredPosition = start.anchoredPosition + relativePos * distance * .5f;
-        image.localEulerAngles = new Vector3(0, 0, GetAngleFromVectorFloat(relativePos));
-        // the second argument, upwards, defaults to Vector3.up
-        return relativePos;
-    }
-
-    public float GetAngleFromVectorFloat(Vector3 dir)
-    {
-        dir = dir.normalized;
-        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        if (n < 0) n += 360;
-
-        return n;
-    }
-
-    public (RectTransform from, RectTransform to) GetBothRect()
-    {
-        return (rUpCorner, lDownCorner);
+        Debug.Log(holder.rect.yMax);
+        Debug.Log(holder.rect.xMax);
+        float val = (holder.rect.xMax- holder.rect.yMax);
+        Debug.Log(me.rect.width);//base
+        Debug.Log(me.rect.height);//height
+        float hyp = (float)Math.Sqrt(holder.rect.width * holder.rect.width + holder.rect.height * holder.rect.height);
+        Debug.Log(hyp);//length//yes
+        //float arc = Mathf.Asin((hyp / holder.rect.height)); //Atan2(float y, float x)
+        float arc = Mathf.Atan2(holder.rect.width, holder.rect.height);
+        Debug.Log(arc * Mathf.Rad2Deg);//yes
     }
 }
 
