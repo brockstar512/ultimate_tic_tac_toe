77ee0b88f1b1d07a0b0cc1433f27df4e670604e9
brockstar512 <60516979+brockstar512@users.noreply.github.com>
2023-04-30 21:27:01 -0500

    added some scenes

--- Assets/Scripts/Game/LineConfig.cs
+++ Assets/Scripts/Game/LineConfig.cs
@@ -2,24 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.WSA;
 using static Enums;
 
 public class LineConfig : MonoBehaviour
 {
     public AngleType angleType;
-    public RectTransform holder;
+    RectTransform holder;
     RectTransform me;
+
 
     private void Awake()
     {
         holder = this.transform.parent.GetComponent<RectTransform>();
         me = this.transform.GetComponent<RectTransform>();
         float hypotenuse = (float)Math.Sqrt(holder.rect.width * holder.rect.width + holder.rect.height * holder.rect.height);
-        Debug.Log(hypotenuse);//length//yes
+        Debug.Log(hypotenuse);
         float angle = Mathf.Atan2(holder.rect.width, holder.rect.height) * Mathf.Rad2Deg;
-        Debug.Log(angle);//yes
+        Debug.Log(angle);
         me.rotation = Quaternion.Euler(0f, 0f, (int)angleType* angle); ;
         me.sizeDelta = new Vector2(me.sizeDelta.x, hypotenuse);
     }
 }
-
---
+++ Assets/Scripts/Game/TimeManager.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening.Core.Easing;
+using TMPro;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timeTitle;
+    public TextMeshProUGUI time;
+    private float timeRemaining;
+    const float timeCountDown = 2;
+    public bool timerIsRunning = false;
+    Color32 normalColor = new Color32(101, 138, 167, 255);
+    [SerializeField] AudioClip timerFailedSoundFX;
+
+    private void Awake()
+    {
+        timeRemaining = timeCountDown;
+        timerIsRunning = false;
+    }
+    private void Start()
+    {
+
+    }
+
+    private void OnDestroy()
+    {
+
+
+    }
+
+    public void StartTimer()
+    {
+        DisplayTime(timeCountDown);
+
+        timeRemaining = timeCountDown;
+
+        // Starts the timer automatically   
+        timerIsRunning = true;
+    }
+    void Update()
+    {
+        if (timerIsRunning)
+        {
+            if (timeRemaining >= 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+        }
+    }
+
+    void MarkCellTimeSuccess()
+    {
+        timerIsRunning = false;
+    }
+    void StopTimer()
+    {
+        timerIsRunning = false;
+    }
+
+    void MarkCellTimeFail()
+    {
+
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        TimeSpan timeLeft = TimeSpan.FromSeconds(timeToDisplay);
+        time.text = string.Format("{0:00}:{1:00}", timeLeft.Seconds, timeLeft.Milliseconds);
+        if (timeToDisplay < 0)
+        {
+            MarkCellTimeFail();
+            time.text = string.Format("{0:00}:{1:00}", 00, 00);
+            time.text = "00:00";
+            timerIsRunning = false;
+            timeRemaining = timeCountDown;
+        }
+    }
+}
